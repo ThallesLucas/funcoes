8bcc655693712d937837d0bcf6fdb5e9dc80f9c1
ThallesLucas <90980021+ThallesLucas@users.noreply.github.com>
2021-11-12 21:59:56 -0300

    Add files via upload

--- Atividade 8 ER2/script.js
+++ Atividade 8 ER2/script.js
@@ -1,12 +1,12 @@
-
 let num = parseInt(prompt("Quantos números você deseja calcular? de 1 à 4: "))
-let media = 0
 
-for(let i = 0; i<num; i++){
-    let num = parseInt(prompt(`Digite o numero ${i+1}: `));
-    media+= num;
+function media(num){
+    let media = 0
+    for(let i = 0; i<num; i++){
+        let num = parseInt(prompt(`Digite o numero ${i+1}: `));
+        media+= num;
+    }
+    return media/num;
 }
 
-media = media/num;
-
-alert(`A média é ${media}`);
+alert(`A média é ${media(num)}`);
--- Atividade 8 ER3/script.js
+++ Atividade 8 ER3/script.js
@@ -1,12 +1,17 @@
 
 let num1 = parseInt(prompt("Digite o valor inicial: "))
 let num2 = parseInt(prompt("Digite o valor final: "))
-let soma = 0
 
-for(let i = num1; i <= num2; i++){
-        if(i%2!==0){
-                soma += i
+
+function soma(num1, num2){
+        let soma = 0
+        for(let i = num1; i <= num2; i++){
+                if(i%2!==0){
+                        soma += i
+                }
         }
+        return soma
 }
 
-alert(`A soma é ${soma}`)
+
+alert(`A soma é ${soma(num1, num2)}`)
